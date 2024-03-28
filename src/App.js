121c855ac18b1c472e73e464ccdd58063a479a80
mirielimacedo <mirielimacedo@gmail.com>
2024-03-28 17:25:14 +0800

    Revert "test error"
    This reverts commit 3c19e9c3bd3769223e354f42a37553ff96f3dd7d.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-import React, {component, useState} from "react";
-import WeatherSearch from './WeatherSearch';
-import WeatherTemperature from "./WeatherTemperature";
 import './App.css';
+import WeatherSearch from './WeatherSearch';
 
 function App() {
-  let [city, setCity] = useState("");
-  
-   function handleCityChange(newCity) {
-    setCity(newCity); // Atualiza o estado com a nova cidade
-  }
-  
   return (
     <div className="App">
       <header className="App-header">
-        <WeatherSearch/>
-        {city && <WeatherTemperature city={city} />}
+      <WeatherSearch/>
+       
        <footer>
-        <p>
+ <p>
           <a
-            className="App-link"
-            href="https://github.com/mirielimacedo/my-app-weather"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Open-source code
-          </a>
-          <small className="App-small">, by </small> 
-          <a className="App-link" href=" https://www.linkedin.com/feed/"
+          className="App-link"
+          href="https://github.com/mirielimacedo/my-app-weather"
           target="_blank"
-            rel="noopener noreferrer">  Mirieli Maria </a>
-          <small className="App-small"> from </small>
-          <a className="App-link" href="https://www.shecodes.io/"
-          target="_blank"
-            rel="noopener noreferrer">   SheCodes</a>
+          rel="noopener noreferrer"
+        >
+         Open-source code
+        </a>
+       <small className="App-small">, by </small> 
+         <a className="App-link" href=" https://www.linkedin.com/feed/"
+        target="_blank"
+          rel="noopener noreferrer">  Mirieli Maria </a>
+        <small className="App-small"> from </small>
+         <a className="App-link" href="https://www.shecodes.io/"
+         target="_blank"
+          rel="noopener noreferrer">   SheCodes</a>
         </p>
         
        </footer>
