4c6faae9136ae36d6380e07de03cce4d750de68b
mirielimacedo <mirielimacedo@gmail.com>
2024-03-28 17:49:24 +0800

    applied prettier to code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import WeatherSearch from "./WeatherSearch";
 import WeatherTemperature from "./WeatherTemperature";
 
-import './App.css';
-
+import "./App.css";
 
 function App() {
- let [city, setCity] = useState(""); // Estado para guardar a cidade
- function handleCityChange(newCity) {
+  let [city, setCity] = useState(""); // Estado para guardar a cidade
+  function handleCityChange(newCity) {
     setCity(newCity); // Atualiza o estado com a nova cidade
   }
-  
+
   return (
     <div className="App">
-     <div className="App">
-  <header className="App-header">
-  <h1>Weather Application</h1>
-  <main>
-  
-    <WeatherSearch onCityChange={handleCityChange}/>
-     {city && <WeatherTemperature city={city} />}
-  </main>
-   <footer>
-    <p>
-      <a className="App-link" href="https://github.com/mirielimacedo/my-app-weather" target="_blank" rel="noopener noreferrer">Open-source code</a>
-      <small className="App-small">, by </small> 
-      <a className="App-link" href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer">Mirieli Maria</a>
-      <small className="App-small"> from </small>
-      <a className="App-link" href="https://www.shecodes.io/" target="_blank" rel="noopener noreferrer">SheCodes</a>
-    </p>
-  </footer>
-  </header>
-  <main>
-   
-  </main>
-  
-</div>
-  
+      <div className="App">
+        <header className="App-header">
+          <h1>Weather Application</h1>
+          <main>
+            <WeatherSearch onCityChange={handleCityChange} />
+            {city && <WeatherTemperature city={city} />}
+          </main>
+          <footer>
+            <p>
+              <a
+                className="App-link"
+                href="https://github.com/mirielimacedo/my-app-weather"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Open-source code
+              </a>
+              <small className="App-small">, by </small>
+              <a
+                className="App-link"
+                href="https://www.linkedin.com/feed/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Mirieli Maria
+              </a>
+              <small className="App-small"> from </small>
+              <a
+                className="App-link"
+                href="https://www.shecodes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                SheCodes
+              </a>
+            </p>
+          </footer>
+        </header>
+        <main></main>
+      </div>
     </div>
   );
 }
