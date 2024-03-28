9a4422b80d354f1daa4d13a4be224c9c426c87c8
mirielimacedo <mirielimacedo@gmail.com>
2024-03-28 16:21:08 +0800

    re-added weather temperature component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
+import React, {component, useState} from "react";
+
+import WeatherSearch from "./WeatherSearch";
+import WeatherTemperature from "./WeatherTemperature";
+
 import './App.css';
-import WeatherSearch from './WeatherSearch';
+
 
 function App() {
+ let [city, setCity] = useState(""); // Estado para guardar a cidade
+ function handleCityChange(newCity) {
+    setCity(newCity); // Atualiza o estado com a nova cidade
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-      <WeatherSearch/>
-       
-       <footer>
- <p>
-          <a
-          className="App-link"
-          href="https://github.com/mirielimacedo/my-app-weather"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Open-source code
-        </a>
-       <small className="App-small">, by </small> 
-         <a className="App-link" href=" https://www.linkedin.com/feed/"
-        target="_blank"
-          rel="noopener noreferrer">  Mirieli Maria </a>
-        <small className="App-small"> from </small>
-         <a className="App-link" href="https://www.shecodes.io/"
-         target="_blank"
-          rel="noopener noreferrer">   SheCodes</a>
-        </p>
-        
-       </footer>
-       
-
-      
-       
-      </header>
+     <div className="App">
+  <header className="App-header">
+  <h1>Weather Application</h1>
+  <main>
+  
+    <WeatherSearch onCityChange={handleCityChange}/>
+     {city && <WeatherTemperature city={city} />}
+  </main>
+   <footer>
+    <p>
+      <a className="App-link" href="https://github.com/mirielimacedo/my-app-weather" target="_blank" rel="noopener noreferrer">Open-source code</a>
+      <small className="App-small">, by </small> 
+      <a className="App-link" href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer">Mirieli Maria</a>
+      <small className="App-small"> from </small>
+      <a className="App-link" href="https://www.shecodes.io/" target="_blank" rel="noopener noreferrer">SheCodes</a>
+    </p>
+  </footer>
+  </header>
+  <main>
+   
+  </main>
+  
+</div>
+  
     </div>
   );
 }
